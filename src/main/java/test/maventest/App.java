b552c86ac075c50949a7133acbd6565e0e5d8c3b
gujiahong <13435748540@139.com>
2015-06-02 11:40:43 +0800

    t

--- src/main/java/test/maventest/App.java
+++ src/main/java/test/maventest/App.java
@@ -9,8 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World222!" );
-        System.out.println( "Hello World333!" );
-        System.out.println( "Hello World4444!" );
     }
 }
